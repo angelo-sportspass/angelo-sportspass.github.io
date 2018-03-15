f498ebe06253c1ae56f8f35d7f893aad9806704b
Angelo Gabisan <angelo@sportspass.com.au>
2018-03-15 15:36:43 +0800

    files

---
+++ js/demo.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#fff',
+    lineColor: '#fff'
+  });
+  // var intro = document.getElementById('intro');
+  // intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
+
+/**
+* @element a
+* @bind Smooth Scroll
+*/
+$(document).ready(function(){
+    $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
